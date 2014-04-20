bba48be2f5b6eccca1594b4f58bf6393d172b414
Melody Mustaine <mel.mustaine@gmail.com>
2014-04-20 15:36:36 -0700

    add project owner + admin roles

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -22,6 +22,10 @@ class ProjectsController < ApplicationController
 
   # GET /projects/1/edit
   def edit
+    @project = Project.find(params[:id])
+    if current_user.admin == false && current_user.id != @project.user_id
+      redirect_to @project
+    end
   end
 
   # POST /projects
@@ -43,24 +47,30 @@ class ProjectsController < ApplicationController
   # PATCH/PUT /projects/1
   # PATCH/PUT /projects/1.json
   def update
-    respond_to do |format|
-      if @project.update(project_params)
-        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
+    if current_user.admin? || current_user.id == @project.user_id
+      respond_to do |format|
+        if @project.update(project_params)
+          format.html { redirect_to @project, notice: 'Project was successfully updated.' }
+          format.json { head :no_content }
+        else
+          format.html { render action: 'edit' }
+          format.json { render json: @project.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to @project
     end
   end
 
   # DELETE /projects/1
   # DELETE /projects/1.json
   def destroy
-    @project.destroy
-    respond_to do |format|
-      format.html { redirect_to projects_url }
-      format.json { head :no_content }
+    if current_user.admin? || current_user.id == @project.user_id
+      @project.destroy
+      respond_to do |format|
+        format.html { redirect_to projects_url }
+        format.json { head :no_content }
+      end
     end
   end
 
@@ -72,6 +82,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name)
+      params.require(:project).permit(:name, :user_id)
     end
 end
